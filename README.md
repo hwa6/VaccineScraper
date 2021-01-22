3457c94629d707bf4a9d2d46611868af6d26fb67
Henry Anderson <handerson5486@gmail.com>
2021-01-22 18:54:27 -0500

    Script, and ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # VaccineScraper
  Screen scraping script developed to quickly book COVID-19 Vaccine
+
+I developed this quick screen scraping python script to automate checking the COVID-19 vaccination webpage for my grandparents. The script opens the desired webpage, checks for an open appointment, and sends an email alerting the desired user if such an opening exists. 
+
+This script utilizes Beautiful Soup 4 and Python 3.9 and was developed in IDLE.
+
+I used a Raspberry Pi to run the script every 10 minutes as a CRONtab, and my grandparents were successfully able to book a spot despite heavy traffic and high demand. 
+
+Please note that for privacy reasons, I have replaced my Gmail auth key with filler text, as well as the desired emails. Please fill these in accordingly if you wish to adapt the script for your needs.
+
+If you use this script, please email me at henrywanderson@engineer.com
+I would be happy to know it helped others!
---
+++ VaccineScraper.py
@@ -0,0 +1,43 @@
+import bs4, requests, smtplib
+print ("Imported")
+
+# ------------------- E-mail list ------------------------
+toAddress = ['EMAIL 1','EMAIL 2','EMAIL 3']
+# --------------------------------------------------------
+
+# Download page
+getPage = requests.get('https://www.ruhealth.org/covid-19-vaccine')
+getPage.raise_for_status() #if error it will stop the program
+
+# Parse text for foods
+rawHTML = bs4.BeautifulSoup(getPage.text, 'html.parser')
+#print(rawHTML.prettify())
+# Count number of "Full" occurrences, then number of "Appointments
+stringHTML = str(rawHTML)
+counter = stringHTML.count('Full.png')
+counter2 = stringHTML.count('AM -') + stringHTML.count(' am –')
+print(counter)
+print(counter2)
+
+
+#Determine if there is an open slot
+openSlot = False
+if (counter/2) != counter2:
+    if(counter/2) < counter2:
+        openSlot = True
+print(openSlot)
+
+#Send email if open slot
+if openSlot == True:
+    conn = smtplib.SMTP('smtp.gmail.com', 587) # smtp address and port
+    conn.ehlo() # call this to start the connection
+    conn.starttls() # starts tls encryption. When password is sent, it will be encrypted.
+    conn.login('YOUR GMAIL', 'YOUR AUTH KEY')
+    conn.sendmail('YOUR GMAIL', toAddress, 'Subject: Potential Opening\n\nHi Amma and Abba\n\nThe script has detected a change, there may be an appointment available.\n\nThis is an automated message.\nVaccine Scraper V1.0')
+    conn.quit()
+    print('Sent notificaton e-mails for the following recipients:\n')
+    for i in range(len(toAddress)):
+        print(toAddress[i])
+    print('')
+else:
+    print('No change detected.')
